14b9ba4d3cd359317e57c5e7a9fbb6d593286bec
bentike <ajadiyusuf73@gmail.com>
2024-01-30 11:30:05 +0100

    served a static file

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 let express = require('express');
 let app = express();
 
+// How to serve a static file
+app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
+
 const myFile = __dirname + "/views/index.html";
 
 
