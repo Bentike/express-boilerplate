f4022aba350c32ae5ad1fe4a8a1335e1871a7cc5
bentike <ajadiyusuf73@gmail.com>
2024-01-30 12:09:37 +0100

    added .env

--- myApp.js
+++ myApp.js
@@ -1,20 +1,26 @@
 let express = require('express');
 let app = express();
 
+// this allow access to .env environment variables
+require('dotenv').config();
+
 // How to serve a static file
 app.use("/public", express.static(__dirname + "/public"));
 
 const myFile = __dirname + "/views/index.html";
 
-
+// Use res.sendFile to send a file
 app.get("/", (req, res) => {
     res.sendFile(myFile);
 });
 
+// Use res.json to send a JSON format data
 app.get("/json", (req, res) => {
-    res.json({
-        "message": "Hello json"
-    });
+    let jsonFile;
+    process.env.MESSAGE_STYLE == "uppercase" ?
+     jsonFile = {"message": "HELLO JSON"} :
+     jsonFile = {"message": "hello json"};
+    res.json(jsonFile);
 })
 
 
